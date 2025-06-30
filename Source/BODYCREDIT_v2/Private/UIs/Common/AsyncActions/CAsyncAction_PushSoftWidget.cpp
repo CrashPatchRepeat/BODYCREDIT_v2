d4b5ea3b9a1ff6aed9195fdbb4e47b5cd2332d48
LHcom <55868914+LHcom@users.noreply.github.com>
2025-06-30 13:39:56 +0900

    팝업 닫기

--- Source/BODYCREDIT_v2/Private/UIs/Common/AsyncActions/CAsyncAction_PushSoftWidget.cpp
+++ Source/BODYCREDIT_v2/Private/UIs/Common/AsyncActions/CAsyncAction_PushSoftWidget.cpp
@@ -11,52 +11,52 @@ UCAsyncAction_PushSoftWidget* UCAsyncAction_PushSoftWidget::PushSoftWidget(const
                                                                            FGameplayTag InWidgetStackTag,
                                                                            bool bFocusOnNewlyPushedWidget)
 {
-	checkf(!InSoftWidgetClass.IsNull(), TEXT("PushSoftWidgetToStack was passed a null soft widget class "));
+    checkf(!InSoftWidgetClass.IsNull(), TEXT("PushSoftWidgetToStack was passed a null soft widget class "));
 
-	if (GEngine)
-	{
-		if (UWorld* World = GEngine->
-			GetWorldFromContextObject(WorldContextObject, EGetWorldErrorMode::LogAndReturnNull))
-		{
-			UCAsyncAction_PushSoftWidget* Node = NewObject<UCAsyncAction_PushSoftWidget>();
-			Node->CachedOwningWorld = World;
-			Node->CachedOwningPC = OwningPlayerController;
-			Node->CachedSoftWidgetClass = InSoftWidgetClass;
-			Node->CachedWidetStackTag = InWidgetStackTag;
-			Node->bCachedFocusOnNewlyPushedWidget = bFocusOnNewlyPushedWidget;
+    if (GEngine)
+    {
+        if (UWorld* World = GEngine->
+            GetWorldFromContextObject(WorldContextObject, EGetWorldErrorMode::LogAndReturnNull))
+        {
+            UCAsyncAction_PushSoftWidget* Node = NewObject<UCAsyncAction_PushSoftWidget>();
+            Node->CachedOwningWorld = World;
+            Node->CachedOwningPC = OwningPlayerController;
+            Node->CachedSoftWidgetClass = InSoftWidgetClass;
+            Node->CachedWidetStackTag = InWidgetStackTag;
+            Node->bCachedFocusOnNewlyPushedWidget = bFocusOnNewlyPushedWidget;
 
-			Node->RegisterWithGameInstance(World);
-			return Node;
-		}
-	}
+            Node->RegisterWithGameInstance(World);
+            return Node;
+        }
+    }
 
-	return nullptr;
+    return nullptr;
 }
 
 void UCAsyncAction_PushSoftWidget::Activate()
 {
-	UCGameInstanceSubsystem* GISubSys = UCGameInstanceSubsystem::Get(CachedOwningWorld.Get());
-	GISubSys->PushSoftWidgetToStackAynsc(CachedWidetStackTag, CachedSoftWidgetClass,
-	                                     [this](EAsyncPushWidgetState InPushState,
-	                                            UCWidgetActivatableBase* PushedWidget)
-	                                     {
-		                                     switch (InPushState)
-		                                     {
-		                                     case EAsyncPushWidgetState::OnCreatedBeforePush:
-			                                     PushedWidget->SetOwningPlayer(CachedOwningPC.Get());
-			                                     OnWidgetCreatedBeforePush.Broadcast(PushedWidget);
-			                                     break;
-		                                     case EAsyncPushWidgetState::AfterPush:
-			                                     AfterPush.Broadcast(PushedWidget);
-			                                     if (bCachedFocusOnNewlyPushedWidget)
-			                                     {
-				                                     if (UWidget* WidgetToFocus = PushedWidget->GetDesiredFocusTarget())
-				                                     {
-					                                     WidgetToFocus->SetFocus();
-				                                     }
-			                                     }
-			                                     SetReadyToDestroy();
-			                                     break;
-		                                     }
-	                                     });
+    UCGameInstanceSubsystem* GISubSys = UCGameInstanceSubsystem::Get(CachedOwningWorld.Get());
+    GISubSys->PushSoftWidgetToStackAynsc(CachedWidetStackTag, CachedSoftWidgetClass,
+                                         [this](EAsyncPushWidgetState InPushState,
+                                                UCWidgetActivatableBase* PushedWidget)
+                                         {
+                                             switch (InPushState)
+                                             {
+                                             case EAsyncPushWidgetState::OnCreatedBeforePush:
+                                                 PushedWidget->SetOwningPlayer(CachedOwningPC.Get());
+                                                 OnWidgetCreatedBeforePush.Broadcast(PushedWidget);
+                                                 break;
+                                             case EAsyncPushWidgetState::AfterPush:
+                                                 AfterPush.Broadcast(PushedWidget);
+                                                 if (bCachedFocusOnNewlyPushedWidget)
+                                                 {
+                                                     if (UWidget* WidgetToFocus = PushedWidget->GetDesiredFocusTarget())
+                                                     {
+                                                         WidgetToFocus->SetFocus();
+                                                     }
+                                                 }
+                                                 SetReadyToDestroy();
+                                                 break;
+                                             }
+                                         });
 }
---
+++ Source/BODYCREDIT_v2/Public/UIs/Market/CInsuffcientGoldWidget.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UIs/Common/CWidgetActivatableBase.h"
+#include "CInsuffcientGoldWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+
+class BODYCREDIT_V2_API UCInsuffcientGoldWidget : public UCWidgetActivatableBase
+{
+    GENERATED_BODY()
+};
